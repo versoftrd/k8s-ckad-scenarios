c6ce63ef994f2f46a6385d572fa74695b6795112
Francisco <fetaverasl@gmail.com>
2023-03-08 18:55:43 -0400

    Ajustes a la descripcion y nuevas pruebas del verify

--- ckad-preparation/step15/verify.sh
+++ ckad-preparation/step15/verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 kubectl get cm configmap-web-moon-html -n moon
-#status=$(k get pods -n moon | grep web-moon | awk 'FNR == 1 {print $3}')
-#if [ "$status" != "Running" ]; then exit 1; fi
+status=$(k get pods -n moon | grep web-moon | awk 'FNR == 1 {print $3}')
+if [ "$status" != "Running" ]; then exit 1; fi
 
