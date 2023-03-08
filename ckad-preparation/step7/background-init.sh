35a0f6b1fa2c621d4e90c667d82d363dca18cc8b
Ivan Lopez <ivan_lopez@bhdleon.com.do>
2023-03-08 16:32:39 -0500

    Step 7 is fixed

--- ckad-preparation/step7/background-init.sh
+++
@@ -1,4 +0,0 @@
-# Crear folders o archivos
-mkdir -p /opt/course/
-cp secret-handler.yaml /opt/course/secret-handler.yaml
-# Ejecutar scripts iniciales
--- ckad-preparation/step7/verify.sh
+++ ckad-preparation/step7/verify.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-kubectl -n moon exec secret-handler -- env | grep SECRET1
-kubectl -n moon exec secret-handler -- find /tmp/secret2
-kubectl -n moon exec secret-handler -- cat /tmp/secret2/key
-# sh /opt/course/2/pod1-status-command.sh;
+
+
+
+if [ -f /opt/course/skips/pregunta7.txt ]; then
+    echo "Ejecución correcta."
+    # Notificar al server resultado
+    archivo=$(cat /opt/course/team/equipo.txt)
+    json="{\"team\":\"$archivo\", \"question\":7, \"result\":\"skip\"}"
+    curl -X POST -H "Content-Type: application/json" -d "$json" https://api-dev.bhd.com.do/killer-coda/result >/dev/null 2>&1 &
+else
+    kubectl -n moon exec secret-handler -- env | grep SECRET1
+    kubectl -n moon exec secret-handler -- find /tmp/secret2
+    kubectl -n moon exec secret-handler -- cat /tmp/secret2/key
+    # sh /opt/course/2/pod1-status-command.sh;
+
+    # Notificar al server resultado
+    archivo=$(cat /opt/course/team/equipo.txt)
+    json="{\"team\":\"$archivo\", \"question\":7, \"result\":\"success\"}"
+    curl -X POST -H "Content-Type: application/json" -d "$json" https://api-dev.bhd.com.do/killer-coda/result >/dev/null 2>&1 &
+fi
