02afec634e14b53e57ac8e0dfe210171d7ab5ad7
fetaverasl <39752327+fetaverasl@users.noreply.github.com>
2023-03-09 15:57:20 -0400

    Update background-init.sh

--- ckad-preparation/step4/background-init.sh
+++ ckad-preparation/step4/background-init.sh
@@ -1,5 +1,4 @@
 # Crear folders o archivos
-mkdir -p /opt/course/4
 
 # Ejecutar scripts iniciales
-kubectl apply -f my-happy-shop.yaml --namespace=saturn
+kubectl apply -f /opt/course/my-happy-shop.yaml --namespace=saturn
