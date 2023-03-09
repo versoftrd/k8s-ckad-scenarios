30ca1be129cf40f757a5e4c0aecca31d2b858244
fetaverasl <39752327+fetaverasl@users.noreply.github.com>
2023-03-09 15:16:42 -0400

    Update background-init.sh

--- ckad-preparation/step5/background-init.sh
+++ ckad-preparation/step5/background-init.sh
@@ -1,7 +1,6 @@
 # Crear folders o archivos
-mkdir -p /opt/course/5
 
 # Ejecutar scripts iniciales
-kubectl apply -f api-new-c32.yaml
-kubectl apply -f api-new-c64.yaml
-kubectl apply -f api-new-c64-error.yaml
+kubectl apply -f /opt/course/api-new-c32.yaml
+kubectl apply -f /opt/course/api-new-c64.yaml
+kubectl apply -f /opt/course/api-new-c64-error.yaml
