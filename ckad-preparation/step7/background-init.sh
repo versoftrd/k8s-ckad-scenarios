e9a0fcf4ab24f307fab7ef0b468dcb6d3970538e
Ivan Lopez <ivan_lopez@bhdleon.com.do>
2023-03-08 23:11:07 -0500

    step07 updated

--- ckad-preparation/step7/background-init.sh
+++ ckad-preparation/step7/background-init.sh
@@ -1,3 +1,3 @@
 cp /opt/course/secret-handler.yaml /opt/course/07/secret-handler.yaml
 cp /opt/course/secret2.yaml /opt/course/07/secret2.yaml
-kubectl apply -f /opt/course/06/secret-handler.yaml
+kubectl apply -f /opt/course/07/secret-handler.yaml
