d95bf8f01a4c7ecee9a8d4f2f82741efe1898553
Ivan Lopez <ivan_lopez@bhdleon.com.do>
2023-03-09 13:50:58 -0500

    Step07 is fixed

--- ckad-preparation/step7/verify.sh
+++ ckad-preparation/step7/verify.sh
@@ -2,8 +2,6 @@
 
 # Namespace a validar
 ns="moon"
-# Secreto a validar
-secret1="secret1"
 
 if [ -f /opt/course/skips/pregunta7.txt ]; then
     echo "Ejecución correcta."
@@ -12,9 +10,10 @@ if [ -f /opt/course/skips/pregunta7.txt ]; then
     json="{\"team\":\"$archivo\", \"question\":7, \"result\":\"skip\"}"
     curl -X POST -H "Content-Type: application/json" -d "$json" https://api-dev.bhd.com.do/killer-coda/result >/dev/null 2>&1 &
 else
-    if ! kubectl get secret "$secret1" -n "$ns" >/dev/null 2>&1 ; then
-    echo "El secreto $secret1 no existe en el namespace $ns"
+    if [! kubectl get secret "secret1" -n "$ns" >/dev/null 2>&1 ] && [! kubectl get secret "secret2" -n "$ns" >/dev/null 2>&1 ]; then
+    echo "El secreto no existe en el namespace $ns"
     exit 1
+
     fi
 
     # Notificar al server resultado
